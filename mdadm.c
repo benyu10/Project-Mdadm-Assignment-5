99e1f5e5d6a2896d5c7f472bcd4a6ddd4180adca
Ben <yubenjamin0820@gmail.com>
2021-04-30 06:06:51 -0700

    finished all functinos

--- mdadm.c
+++ mdadm.c
@@ -1,22 +1,194 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-
+#include <stdlib.h>
 #include "mdadm.h"
 #include "jbod.h"
+#include "net.h"
 
+
+uint32_t encode_operation(jbod_cmd_t cmd, int disk_num, int block_num)
+{
+
+  uint32_t op = cmd << 26 | disk_num << 22 | block_num;
+  return op;
+}
+
+void translate_address(uint32_t linear_address,
+		       int *disk_num,
+		       int *block_num,
+		       int *offset)
+{
+  int block_remainder=0;
+  *disk_num = linear_address / JBOD_DISK_SIZE;
+  block_remainder = linear_address % JBOD_DISK_SIZE;
+  *block_num = block_remainder / JBOD_BLOCK_SIZE;
+  *offset = block_remainder % JBOD_BLOCK_SIZE;  
+}
+
+
+int seek(int disk_num, int block_num)
+{
+  uint32_t op;
+  uint32_t ops;  
+  op = encode_operation(JBOD_SEEK_TO_DISK, disk_num, 0);
+  ops = encode_operation(JBOD_SEEK_TO_BLOCK, 0, block_num);
+  jbod_client_operation(op, NULL);
+  jbod_client_operation(ops, NULL);
+  return 1;
+}
+int findmin(int n, int n2)
+{
+  if (n>n2)
+    {
+      return n2;
+    }
+  else if (n==n2)
+    {
+      return n;
+    }
+  else
+    {
+      return n;
+    }
+}
+
+int mounted = 0; 
 int mdadm_mount(void) {
+   uint32_t op = encode_operation(JBOD_MOUNT, 0, 0); // pass zero's because its default
+  int value = jbod_client_operation(op, NULL);
+  if (value == 0)
+    {
+       mounted = 1;
+      return 1;
+    }
   return -1;
 }
 
 int mdadm_unmount(void) {
+  uint32_t last_op = encode_operation(JBOD_UNMOUNT,0, 0); //last command called so its on last disk, last block
+  int value = jbod_client_operation(last_op, NULL);
+  if (value == 0)
+    {
+      mounted =0;
+      return 1;
+    }
   return -1;
 }
 
 int mdadm_read(uint32_t addr, uint32_t len, uint8_t *buf) {
+  int disk_num, block_num, offset;
+  if (mounted == 0 || len > 1024 || (len && buf == NULL) || addr+ len >= (JBOD_NUM_DISKS * JBOD_DISK_SIZE)) //test cases
+  {
+    return -1; 
+  }
+ 
+ 
+  int total = len;
+  int data_read = 0;
+  int curr_addy = addr;
+  int sumDR = 0;
+  int counter = 0;
+  uint8_t buf1[256];
+  while (curr_addy<addr+len){
+    
+    translate_address(curr_addy, &disk_num, &block_num, &offset);
+    seek(disk_num, block_num);
+  
+    if (cache_enabled()==true)
+        {
+
+          if ( cache_lookup(disk_num, block_num, buf1) == 1)
+	    {
+	      memcpy(buf,buf1,256-offset);
+	      return 1;
+	    }
+	} 
+
+    uint32_t op = encode_operation(JBOD_READ_BLOCK, 0, 0);
+    jbod_client_operation(op,buf1);
+    cache_insert(disk_num, block_num, buf1);
+    
+   if (counter == 0) //first block bc current block is equal to first block num   
+    {
+      memcpy(buf+sumDR, buf1+offset, findmin((JBOD_BLOCK_SIZE - offset),len));
+      data_read = findmin((JBOD_BLOCK_SIZE - offset),len);
+      counter += 1; //increments the counter so not in first block anymore
+      //if last block, then disk + 1 to go to next disk  	
+    }
+   else if (total < JBOD_BLOCK_SIZE)//last block, read whats left of the data by using x
+    {
+      memcpy(buf+sumDR, buf1, total);
+      data_read = total;
+    }
+  else
+    {
+      memcpy(buf+sumDR, buf1, JBOD_BLOCK_SIZE); //middle blocks, so read full block size. offset is zero, new block
+      data_read = JBOD_BLOCK_SIZE; 
+    }
+   
+     sumDR = sumDR + data_read;
+     total = total - data_read;//keeps track of the bytes left to read
+     curr_addy = curr_addy + data_read;
+  }
   return len;
 }
 
 int mdadm_write(uint32_t addr, uint32_t len, const uint8_t *buf) {
-  return len;
-}
+  if (mounted == 0 || len > 1024 || (len && buf == NULL) || (addr+len) > (JBOD_NUM_DISKS * JBOD_DISK_SIZE)) //test case
+  {
+    return -1;
+  }
+  int disk_num, block_num, offset;
+ 
+  int total = len;
+  int data_read = 0;
+  int curr_addy = addr;
+  int sumDR = 0;
+  int counter = 0;
+  uint8_t buf1[256];
+
+  while (curr_addy<addr+len){ //keeps the loop going until the address matches
+
+    translate_address(curr_addy, &disk_num, &block_num, &offset); //finds the disk block and offset num
+    seek(disk_num, block_num); //looks for right disk and block num
+    uint32_t op = encode_operation(JBOD_READ_BLOCK, 0, 0); //reads the block
+    jbod_client_operation(op,buf1); //sets buf to contents of what was read
+    
+   if (counter == 0) //first block bc current block is equal to first block num                                                                      
+    {
+      memcpy(buf1+offset,buf+sumDR ,findmin((JBOD_BLOCK_SIZE - offset),len)); //writes the user data len into buf1 from buf
+      seek(disk_num, block_num); 
+      uint32_t ops = encode_operation(JBOD_WRITE_BLOCK, 0, 0); //writes the contents into disk
+      jbod_client_operation(ops,buf1);
+      data_read = findmin((JBOD_BLOCK_SIZE - offset),len); //calculates what was written
+     
+      counter += 1; //increments the counter so not in first block anymore                                                                           
+    }
+   else if (total < JBOD_BLOCK_SIZE)//last block, read whats left of the data by using x                                                              
+    {
+      
+      memcpy(buf1, buf+sumDR, total);
+      seek(disk_num, block_num);
+      uint32_t ops = encode_operation(JBOD_WRITE_BLOCK, 0, 0);
+      jbod_client_operation(ops,buf1);
+
+      data_read = total;
+    }
+  else
+    {
+      memcpy(buf1, buf+sumDR, JBOD_BLOCK_SIZE); //middle blocks, so read full block size. offset is zero, new block                                   
+      seek(disk_num, block_num);
+      uint32_t ops = encode_operation(JBOD_WRITE_BLOCK, 0, 0);
+      jbod_client_operation(ops,buf1);
+  
+      data_read = JBOD_BLOCK_SIZE;
+    }
+
+   sumDR = sumDR + data_read; //the sum of all data read by adding it together
+     total = total - data_read;//keeps track of the bytes left to read by subtracting total by data read                                                                                
+     curr_addy = curr_addy + data_read; //calculates the address by adding data already read
+  }
+
+ return len;
+ }
--- net.c
+++ net.c
@@ -17,35 +17,132 @@ int cli_sd = -1;
 /* attempts to read n bytes from fd; returns true on success and false on
  * failure */
 static bool nread(int fd, int len, uint8_t *buf) {
-  return false;
+  int data_read = 0;
+  while (data_read < len)
+    {
+      int n = read(fd, &buf[data_read], len - data_read);
+      if (n < 0)
+	{
+	  return false;
+	}
+      data_read += n;
+    }
+  return true;
 }
 
 /* attempts to write n bytes to fd; returns true on success and false on
  * failure */
 static bool nwrite(int fd, int len, uint8_t *buf) {
-  return false;
+  int data_written =0;
+  while (data_written < 0)
+  {
+    int n = write(fd, &buf[data_written], len - data_written);
+    if (n < 0)
+      {
+	return false;
+      }
+    data_written += n;
+  }
+  return true;
 }
 
 /* attempts to receive a packet from fd; returns true on success and false on
  * failure */
 static bool recv_packet(int fd, uint32_t *op, uint16_t *ret, uint8_t *block) {
+  uint16_t len;
+  uint8_t header[HEADER_LEN];
+
+  if (!nread(fd, HEADER_LEN, header))
+    {
+      return false;
+    }
+
+  int offset = 0;
+  memcpy(&len, header + offset, sizeof(len));
+  offset += sizeof(len);
+  memcpy(op, header + offset, sizeof(*op));
+  offset += sizeof(*op);
+  memcpy(ret, header + offset, sizeof(*ret));
+
+  len = ntohs(len);
+  *op = ntohl(*op);
+  *ret = ntohs(*ret);
+
+  if(len == 264) //checks to see if its a read operation, if it is then read extra 256
+    {
+      if (nread(fd, 256, block) == false)
+	{
+	  return false;
+	}
+    }
+
+  return true;
 }
 
 /* attempts to send a packet to sd; returns true on success and false on
  * failure */
 static bool send_packet(int sd, uint32_t op, uint8_t *block) {
+
+  uint8_t header[264];
+  uint16_t len = HEADER_LEN;
+  uint16_t opcode = op >> 26;
+
+  if (opcode == JBOD_WRITE_BLOCK)
+    {
+      len += 256;
+    }
+  
+  uint16_t new_len = htons(len);
+  uint32_t new_op = htonl(op);
+ 
+  //constructing the packet
+  memcpy(header, &new_len, sizeof(new_len));
+  memcpy(header+sizeof(&new_len), &new_op, sizeof(new_op));
+
+  if (opcode == JBOD_WRITE_BLOCK)
+    {
+      memcpy(header+ HEADER_LEN, block, 256);
+    }
+  //writing the packet using nwrite
+  return nwrite(sd, len, header);
 }
+
 
 /* attempts to connect to server and set the global cli_sd variable to the
  * socket; returns true if successful and false if not. */
 bool jbod_connect(const char *ip, uint16_t port) {
+  
+  cli_sd = socket(AF_INET, SOCK_STREAM, 0);
+  if (cli_sd == -1)
+    {
+      return false;
+    }
+
+  struct sockaddr_in caddr;
+  
+  caddr.sin_family = AF_INET;
+  caddr.sin_port = htons(port);
+
+  inet_aton(ip, &caddr.sin_addr);
+  connect(cli_sd, (const struct sockaddr *)&caddr, sizeof(caddr));
+  if (connect(cli_sd, (const struct sockaddr *)&caddr, sizeof(caddr))== 0) 
+    {
+      return true;
+    }
+  return false;
 }
 
 /* disconnects from the server and resets cli_sd */
 void jbod_disconnect(void) {
+  close(cli_sd);
+  cli_sd = -1;
 }
 
 /* sends the JBOD operation to the server and receives and processes the
  * response. */
 int jbod_client_operation(uint32_t op, uint8_t *block) {
-}
+
+  uint16_t ret; 
+  send_packet(cli_sd, op, block);
+  recv_packet(cli_sd, &op, &ret, block);
+  return ret;
